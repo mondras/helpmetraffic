b2b1816933cb2d4fd60b1086347980939eaeb40b
Alfonso Colunga <poncho2489@gmail.com>
2011-05-09 21:44:50 -0500

    Versión donde el cliente envía cada 15 segs llamada a /update. La respuesta consiste en un arreglo de tripletas {"lat":, "lng":, "vel":} Se usa una vista que guarda las coordenadas, el tiempo y la velocidad. Se usa una lista que sólo retorna los elementos de la vista cuyo tiempo no tenga una diferencia mayor a 5 minutos del momento donde se consulta.

--- app.js
+++ app.js
@@ -54,17 +54,60 @@ app.post('/register', function(req, res){
 app.post('/update', function(req,res) {
   var cradle = require('cradle');
   var db = new(cradle.Connection)().database('helpmetraffic');
+  var id = req.body.id;
+  var rev;
+  var lat1, lat2, lng1, lng2, time1, time2, vel;
   //find user
-  db.get(req.body.id, function (err,doc) { 
-      console.log("Retriveo:"+doc);
+  db.get(id, function (err,doc) {
+      // update user
+      if(doc==undefined){ res.send('{response:"ERROR"}');}
+      if(!err && doc!= undefined){
+        rev = doc._rev;
+        lat1 = doc.lat;
+        lng1 = doc.lng;
+        time1 = doc.time;
+        lat2 = req.body.latitude;
+        lng2 = req.body.longitude;
+        time2 = req.body.time;
+        if (lat1 && lng1 && time1){
+          if ((time2 - time1) <= 60000){
+            // calculate distance
+            var R = 6371; 
+            var dLat = (lat2-lat1)*Math.PI / 180;
+            var dLon = (lng2-lng1)*Math.PI / 180; 
+            var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                    Math.cos(lat1*Math.PI / 180) * Math.cos(lat2*Math.PI / 180) * 
+                    Math.sin(dLon/2) * Math.sin(dLon/2); 
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+            var d = R * c
+            vel = d/(time2-time1);
+            console.log("velocidad "+vel);
+          }
+        } 
+        db.save(id, rev, {
+          device:doc.device,
+          sex: doc.sex,
+          yob: doc.yob,
+          lat: lat2,
+          lng: lng2,
+          time: time2,
+          vel: vel
+        }, function (error, response) {
+          if (error) {
+	    console.log("Error al guardar nueva pos");
+            res.send(error);
+          } else {
+            // send all users' info
+            var now = (new Date()).getTime();
+            db.list('view_positions/active/all', {now: now}, function(view_err, view_res){
+            //db.view('view_positions/all', function(view_err, view_res){
+              console.log("la lista regresa "+view_res);
+              res.send(JSON.stringify(view_res));
+            });
+          }
+        });   
+      }
     });
-    
-  //Update it
-  
-  //Agregate it?
-  
-  //Send a list of all other users
-  res.send('');
 });
 
 // Only listen on $ node app.js
@@ -72,4 +115,12 @@ app.post('/update', function(req,res) {
 if (!module.parent) {
   app.listen(3000);
   console.log("Express server listening on port %d", app.address().port);
+}
+
+
+
+if (typeof(Number.prototype.toRad) === "undefined") {
+  Number.prototype.toRad = function() {
+    return this * Math.PI / 180;
+  }
 }
